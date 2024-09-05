97810e65d417e250a750c011b1e00619f7cfe2e8
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-05 17:39:18 +0800

    Put in a button to change amount of squares

--- javascript.js
+++ javascript.js
@@ -1,16 +1,38 @@
 const container = document.querySelector('.container')
+let squaresPerSide = 16
+let squareHeight = 6.25
 
-for( let i = 0; i<256;i++){
-    const div = document.createElement('div')
-    div.classList.add('square')
-    div.addEventListener('mouseover', () => {
-        div.style.transition =  'none'
-        div.style.backgroundColor = 'blue'
-    })
-    div.addEventListener('mouseout', () => {
-        div.style.transition =  'background-color 1s'
-        div.style.backgroundColor = 'yellow'})
-    container.appendChild(div)
+//creation of squares
+squareCreation = (numOfSquares,squareHeight) => {
+    for( let i = 0; i<numOfSquares;i++){
+        const div = document.createElement('div')
+        div.classList.add('square')
+        div.style.height = squareHeight+'%'
+    
+        div.addEventListener('mouseover', () => {
+            div.style.transition =  'none'
+            div.style.backgroundColor = 'blue'
+        })
+    
+        div.addEventListener('mouseout', () => {
+            div.style.transition =  'background-color 1s'
+            div.style.backgroundColor = 'yellow'})
+        container.appendChild(div)
+    }
 }
 
+squareCreation(squaresPerSide**2,squareHeight)
 
+
+const sizeBtn = document.querySelector('.size')
+sizeBtn.addEventListener('click', () => {
+    squaresPerSide = prompt('How many squares per side?')
+    if (squaresPerSide > 100) {
+        squaresPerSide = 100
+    } else if (squaresPerSide < 1){
+        squaresPerSide = 1
+    }
+    squareHeight = 100/squaresPerSide
+    container.textContent = ''
+    squareCreation(squaresPerSide**2,squareHeight)
+})
