264c6620f139fe4b7c9508b4d38c10b634d2bfcb
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-05 17:01:58 +0800

    Added hovering effect

--- javascript.js
+++ javascript.js
@@ -3,5 +3,14 @@ const container = document.querySelector('.container')
 for( let i = 0; i<256;i++){
     const div = document.createElement('div')
     div.classList.add('square')
+    div.addEventListener('mouseover', () => {
+        div.style.transition =  'none'
+        div.style.backgroundColor = 'blue'
+    })
+    div.addEventListener('mouseout', () => {
+        div.style.transition =  'background-color 1s'
+        div.style.backgroundColor = 'yellow'})
     container.appendChild(div)
 }
+
+
